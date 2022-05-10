1cd4121e1059275b4bb0a218cb56f85696c8be9c
MementoMorii <b.garic.99@yandex.ru>
2022-05-10 17:13:34 +0300

    Initial commit

---
+++ Lab2PorogFilter/Lab2PorogFilter/PorogFilterService.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab2PorogFilter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    class PorogFilterService
+    {
+
+    }
+}
